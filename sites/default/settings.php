373f8b27e2a899602f1d1ce7da115a112c675a93
Mihail Kachanovskii <nulay@mail.ru>
2017-11-20 23:54:40 +0300

    db settings

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -92,8 +92,7 @@
  *   $db_url = 'mysqli://root@localhost/latinoby_imix';
  *
  */
-#
-$db_url = 'mysql://latinoby_imix:nulay787871@localhost/latinoby_imix';
+$db_url = 'mysql://latinoby_imix:nulay787871@172.30.194.68/latinoby_imix';
 $db_prefix = '';
 
 /**
